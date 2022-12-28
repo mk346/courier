65177b31a701f0941e5891db5cedc6ab2a3d1b49
mk346 <calebkinyuamunene@gmail.com>
2022-12-28 20:12:51 +0300

    first commit

---
+++ includes/login_handler.php
@@ -0,0 +1,43 @@
+<?php
+// require("../config/config.php");
+//declaring variables
+$error_array = array();
+$email = "";
+$password = "";
+$username = "";
+
+if(isset($_POST['submit'])){
+    //cleaning input data before sending it to the database
+    $email = strip_tags($_POST['email']); //strips html tags
+    $email = filter_var($email,FILTER_SANITIZE_EMAIL); //sanitize email
+    $email = str_replace(' ', '',$email); //removes white spaces
+    $_SESSION['email'] = $email;
+
+    $password = strip_tags($_POST['email']);
+    $password = md5($_POST['password']); // encrypt password
+    $_SESSION['password'] = $password;
+
+    // query the database to check if the user exists
+    $check_database = mysqli_query($con, "SELECT * FROM users WHERE email = '$email' AND Password = '$password'");
+    $check_login_query = mysqli_num_rows($check_database);
+
+    if($check_login_query == 1){
+        $row = mysqli_fetch_array($check_database); //store database result in to an array
+        $username = $row['fname'];
+        $_SESSION['username'] = $username;
+
+        header("Location: index.php");
+        exit();
+
+    }
+    else{
+        array_push($error_array, "<span style='color: red;'>Email or Password was Incorrect</span><br>");
+    }
+
+
+}
+
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,43 @@
+<?php
+require("config/config.php");
+require("includes/login_handler.php");
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/0fe3bc1f22.js" crossorigin="anonymous"></script>
+    <title>Login Page</title>
+</head>
+<body>
+    <section class="admin-login">
+        <div class="login-wrapper">
+            <div class="wrapper-title">
+                <span>Courier Management System</span>
+            </div>
+            <form action="login.php" method="POST" class="wrapper-form">
+                <div class="wrapper-row">
+                    <i class="fa-solid fa-envelope"></i>
+                    <input type="email" name="email" class="input-type-1" value="<?php if (isset($_SESSION['email'])) echo $_SESSION['email']; ?>" placeholder="Email" required>
+                </div>
+                <div class="wrapper-row">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" name="password" class="input-type-1" value="<?php if (isset($_SESSION['password'])) echo $_SESSION['password']; ?>" placeholder="Password" required>
+                </div>
+                <div class="wrapper-row">
+                    <p class="error-message"><?php if (in_array("<span style= 'color: red;'>Email or Password was Incorrect</span><br>", $error_array)) echo "<span style= 'color: red;'>Email or Password was Incorrect</span><br>";?></p>
+                </div>
+                <div class="wrapper-row row-button">
+                    <input type="submit" name="submit" value="Login" class="submit-login-1">
+                </div>
+            </form>
+        </div>
+
+    </section>
+
+
+
+</body>
+</html>
