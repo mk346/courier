d39d5f7520dee7552c0385d1b6a344c4d4fe6555
mk346 <calebkinyuamunene@gmail.com>
2024-05-27 23:38:58 +0300

    update-3

--- config/config.php
+++ config/config.php
@@ -1,7 +1,18 @@
 <?php
+
+use Dotenv\Dotenv;
+
+require 'vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+//SET TIMEZONE
 $timezone = date_default_timezone_set("Africa/Nairobi");
-$con = mysqli_connect("127.0.0.1:3307", "root", "Apple@mango", "courier");
-//$con = mysqli_connect("localhost", "root", "", "courier");
+
+//CONNECTION STRING
+$con = mysqli_connect($_ENV['HOSTNAME'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DATABASE']);
+
+
 if (mysqli_connect_errno()){
     echo "Failed to connect". mysqli_connect_errno();
 }
